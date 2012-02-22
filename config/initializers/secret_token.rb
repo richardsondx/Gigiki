028920e83ffcb1d837d0da315defe93c334cea1b
RichardsonDx <richardsondx@gmail.com>
2012-02-22 17:20:34 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gigiki::Application.config.secret_token = 'f5278dffd6c2a7af5144315406b37fe08a4cdda7838e66d4cf40eecfb56be1e33dd7d9145855ec904eca8c1f6afd41330d804dd62b564800f45fe8ef0741fc57'
